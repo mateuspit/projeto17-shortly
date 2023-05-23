1b8a4bf8876077a6bff123f5d5d2e3e4fd6bfd1f
Mateus Barcelos <mateuspit@hotmail.com>
2023-05-23 17:05:51 -0300

    feat: delete by id

--- src/controllers/linksController.js
+++ src/controllers/linksController.js
@@ -1,8 +1,13 @@
-import { createLinkServices, getLinkByIdServices, getLinkByShortUrlServices } from "../services/linksServices.js";
+import { createLinkServices, findUrlByIdServices, deleteLinkServices, getLinkByIdServices, getLinkByShortUrlServices, sameOwnerAnalyseServices } from "../services/linksServices.js";
 
 export async function deleteLinkById(req, res) {
     try {
-        isTheRightUser = await findUrlByUserServices(req.params.id)
+        const urlExist = await findUrlByIdServices(req.params.id);
+        if (!urlExist) return res.sendStatus(401);
+        const isTheRightUser = await sameOwnerAnalyseServices(req);
+        if (!isTheRightUser) return res.sendStatus(401);
+        const deletedLink = await deleteLinkServices(req.body.url);
+        res.status(204).send(deletedLink)
     }
     catch (err) {
         return console.log(err.message);
@@ -13,7 +18,7 @@ export async function getLinkByShortUrl(req, res) {
     try {
         const urlData = await getLinkByShortUrlServices(req.params.shortUrl);
         if (urlData.lenght === 0) return res.sendStatus(404);
-        res.status(200).send(urlData);
+        res.redirect(urlData);
     }
     catch (err) {
         return console.log(err.message);
--- src/services/linksServices.js
+++ src/services/linksServices.js
@@ -1,6 +1,30 @@
 import { db } from "../database/database-connection.js";
 import { nanoid } from 'nanoid'
 
+export async function findUrlByIdServices(id) {
+    try {
+        const urlExists = db.query(`SELECT url FROM urls WHERE "userId"=$1`, [id]);
+        if (urlExists.rows[0].lenght === 0) return false;
+        return true;
+    } catch (err) {
+        return console.log(err.message);
+    }
+
+}
+
+export async function sameOwnerAnalyseServices(req) {
+    try {
+        const existLinkById = await db.query(`SELECT id FROM users
+                                            JOIN urls ON users.id = "userId".urls
+                                            WHERE "url" = $1`, [req.body.url]);
+        if (req.params.id !== existLinkById.rows[0].id) return false;
+        return true;
+    }
+    catch (err) {
+        return console.log(err.message);
+    }
+}
+
 export async function getLinkByShortUrlServices(shortUrl) {
     try {
         const linkData = await db.query(`SELECT id, "shortUrl", "url" FROM url WHERE "shortUrl"=$1`, [shortUrl]);
